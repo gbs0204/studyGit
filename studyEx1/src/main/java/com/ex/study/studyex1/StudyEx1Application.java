c7d5cd680f360544d5bbc187ce56c3daf7673f07
조용진 <kama624@garetail.com>
2022-02-27 15:58:31 +0900

    커밋1

--- studyEx1/src/main/java/com/ex/study/studyex1/StudyEx1Application.java
+++ studyEx1/src/main/java/com/ex/study/studyex1/StudyEx1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyEx1Application {
 
     public static void main(String[] args) {
-
+        // 1
         SpringApplication.run(StudyEx1Application.class, args);
     }
 
