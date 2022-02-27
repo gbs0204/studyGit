265fbb9292876665a7d8f3a19b1799fd9e9140d6
조용진 <kama624@garetail.com>
2022-02-27 15:22:17 +0900

    최초 커밋

---
+++ studyEx1/src/main/java/com/ex/study/studyex1/StudyEx1Application.java
@@ -0,0 +1,14 @@
+package com.ex.study.studyex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEx1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StudyEx1Application.class, args);
+    }
+
+}
---
+++ studyEx1/src/test/java/com/ex/study/studyex1/StudyEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.study.studyex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyEx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
