9d63385b3d395924fb3957b7fab23bc976210fca
조용진 <kama624@garetail.com>
2022-03-06 15:30:05 +0900

    test2

---
+++ studyEx1/src/main/java/com/ex/study/studyex1/java4.java
@@ -0,0 +1,10 @@
+package com.ex.study.studyex1;
+
+public class java4 {
+    public static void main(String[] agm){
+        test1 t1 = new test1();
+        String sumString  = t1.sum("1111", "2222");
+//        system.out.println("dd"+sumString);
+
+    }
+}
--- studyEx1/src/main/java/com/ex/study/studyex1/test1.java
+++ studyEx1/src/main/java/com/ex/study/studyex1/test1.java
@@ -3,7 +3,10 @@ package com.ex.study.studyex1;
 public class test1 {
     //2  333 v 4444  55555 local2
     // 11
-//2
+//2Sti
     //3
     // 4 amend 2 3
+    public String sum(String a, String b){
+        return a+b;
+    }
 }
